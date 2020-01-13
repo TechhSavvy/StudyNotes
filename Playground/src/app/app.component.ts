6bc0ce4c56a262be9f42d8ed31654ffa2f030e48
Tech Savvy <tooensure@gmail.com>
2020-01-13 17:39:09 -0600

    Adding Angular project

---
+++ Playground/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'Playground';
+}
+
+var myString;
+
+myString = "Hello World";
+
+console.log(myString)
