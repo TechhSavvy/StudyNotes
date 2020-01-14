1755c952ceb69bde31400104104ad37c76e91f15
Tech Savvy <tooensure@gmail.com>
2020-01-14 15:01:51 -0600

    updating 1.3 basics content

--- Playground/src/app/app.component.ts
+++ Playground/src/app/app.component.ts
@@ -8,29 +8,3 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Playground';
 }
-
-let myString: string;
-let myNumber: number;
-let myBoolean: any;
-let myArr: string[];
-let numArr: Array<number>;
-
-let strNumTuple: [string, number, number];
-
-myString = "Hello World".slice(0, 3);
-myNumber = 22;
-myBoolean = [true, false, "Tech Savvy", 22];
-myArr = ["Saterday", "Sunday"];
-numArr = [1,2,3,4]
-
-strNumTuple = ["Tech Savvy", 22, 22, ]
-
-let myVoid: void;
-let myNull: null;
-let myUndefined: undefined;
-
-myVoid = undefined;
-myNull = undefined;
-myUndefined = undefined;
-
-console.log(numArr)
--- Playground/src/app/app.module.ts
+++ Playground/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FunctionsComponent } from './functions/functions.component';
+import { TypescriptTypesComponent } from './typescript-types/typescript-types.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FunctionsComponent,
+    TypescriptTypesComponent
   ],
   imports: [
     BrowserModule,
